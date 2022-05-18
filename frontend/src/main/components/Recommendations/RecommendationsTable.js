e3d57cfe6340f1149fd3d0be9dc0c9c7200f28d4
Adrian Lindell <lindell@ucsb.edu>
2022-05-17 20:57:27 -0700

    al - implemented RecommendationsTable and RecommendationsIndexPage

---
+++ frontend/src/main/components/Recommendations/RecommendationsTable.js
@@ -0,0 +1,87 @@
+import OurTable, { _ButtonColumn} from "main/components/OurTable";
+import { useBackendMutation } from "main/utils/useBackend";
+import {  onDeleteSuccess } from "main/utils/UCSBDateUtils"
+// import { useNavigate } from "react-router-dom";
+import { _hasRole } from "main/utils/currentUser";
+
+
+export function cellToAxiosParamsDelete(cell) {
+    return {
+        url: "/api/recommendations",
+        method: "DELETE",
+        params: {
+            code: cell.row.values.code
+        }
+    }
+}
+
+export default function RecommendationsTable({ recommendations, _currentUser }) {
+
+    // const navigate = useNavigate();
+
+    // const editCallback = (cell) => {
+    //     navigate(`/ucsbdates/edit/${cell.row.values.id}`)
+    // }
+
+    // Stryker disable all : hard to test for query caching
+    const _deleteMutation = useBackendMutation(
+        cellToAxiosParamsDelete,
+        { onSuccess: onDeleteSuccess },
+        ["/api/recommendations/all"]
+    );
+    // Stryker enable all 
+
+    // Stryker disable next-line all : TODO try to make a good test for this
+    // const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
+
+    const columns = [
+        {
+            Header: 'Date Needed',
+            accessor: 'dateNeeded', 
+        },
+        {
+            Header: 'Date Requested',
+            accessor: 'dateRequested',
+        },
+        {
+            Header: 'Done?',
+            id: 'isDone', // needed for tests
+            accessor: (row, _rowIndex) => String(row.done) // hack needed for boolean values to show up
+        },
+        {
+            Header: 'Explanation',
+            accessor: 'explanation',
+
+        },
+        {
+            Header: 'Id',
+            id: 'id', // needed for tests
+            accessor: (row, _rowIndex) => String(row.id) // hack needed for boolean values to show up
+        },
+        {
+            Header: 'Professor Email',
+            accessor: 'professorEmail',
+        },
+        {
+            Header: 'Requester Email',
+            accessor: 'requesterEmail',
+        }
+    ];
+
+    const testid = "RecommendationsTable";
+
+    const _columnsIfAdmin = [
+        ...columns,
+        // ButtonColumn("Edit", "primary", editCallback, testid),
+        // ButtonColumn("Delete", "danger", deleteCallback, testid)
+    ];
+
+    // const columnsToDisplay = hasRole(currentUser, "ROLE_ADMIN") ? columnsIfAdmin : columns;
+    const columnsToDisplay = columns;
+
+    return <OurTable
+        data={recommendations}
+        columns={columnsToDisplay}
+        testid={testid}
+    />;
+};
--- frontend/src/main/pages/Recommendations/RecommendationsIndexPage.js
+++ frontend/src/main/pages/Recommendations/RecommendationsIndexPage.js
@@ -1,13 +1,28 @@
+import React from 'react'
+import { useBackend } from 'main/utils/useBackend'; // use prefix indicates a React Hook
+
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import _RecommendationsTable from 'main/components/Recommendations/RecommendationsTable';
+import { useCurrentUser } from 'main/utils/currentUser' // use prefix indicates a React Hook
 
 export default function RecommendationsIndexPage() {
+
+  const _currentUser = useCurrentUser();
+
+  const { data: _recommendations, error: _error, status: _status } =
+    useBackend(
+      // Stryker disable next-line all : don't test internal caching of React Query
+      ["/api/recommendations/all"],
+            // Stryker disable next-line StringLiteral,ObjectLiteral : since "GET" is default, "" is an equivalent mutation
+            { method: "GET", url: "/api/recommendations/all" },
+      []
+    );
+
   return (
     <BasicLayout>
       <div className="pt-2">
         <h1>Recommendations</h1>
-        <p>
-          This is where the index page will go
-        </p>
+        {/* <RecommendationsTable recommendations={recommendations} currentUser={currentUser} /> */}
       </div>
     </BasicLayout>
   )
