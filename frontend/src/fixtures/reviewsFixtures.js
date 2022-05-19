cc67ab1f522ce5d0f4058c006216bd3e75ee857c
Sangita Kunapuli <sangita@169-231-115-160.wireless.ucsb.edu>
2022-05-18 18:35:23 -0700

    sk - created fixtures for ReviewsTable and tested frontend for Reviews using Storybook

---
+++ frontend/src/fixtures/reviewsFixtures.js
@@ -0,0 +1,39 @@
+const reviewsFixtures = {
+    oneReview: {
+        "id": 1,
+        "itemId": 20,
+        "reviewerEmail": "hungry_gaucho@ucsb.edu",
+        "stars": 2,
+        "dateReviewed": "2022-04-15",
+        "comments": "was very mid"
+    },
+    threeMenuReviews: [
+        {
+          "id": 1,
+          "itemId": 44,
+          "reviewerEmail": "gaucho@ucsb.edu",
+          "stars": 3,
+          "dateReviewed": "2022-04-05",
+          "comments": "stupid meal"
+        },
+        {
+          "id": 2,
+          "itemId": 3,
+          "reviewerEmail": "gaucho@ucsb.edu",
+          "stars": 4,
+          "dateReviewed": "2022-04-20",
+          "comments": "WAY too much to eat"
+        },
+        {
+          "id": 4,
+          "itemId": 3,
+          "reviewerEmail": "helpful@ucsb.edu",
+          "stars": 5,
+          "dateReviewed": "2022-04-20",
+          "comments": "very yummy"
+        }
+      ]
+};
+
+
+export { reviewsFixtures };
