63d53648d0bb5293e4efcb5813a2832cd2ee920c
ljvcs <72827930+ljvcs@users.noreply.github.com>
2022-05-18 18:27:46 -0700

    lv - created ArticlesTable component

---
+++ frontend/src/main/components/Articles/ArticlesTable.js
@@ -0,0 +1,66 @@
+import OurTable, { ButtonColumn } from "main/components/OurTable";
+import { useBackendMutation } from "main/utils/useBackend";
+import { cellToAxiosParamsDelete, onDeleteSuccess } from "main/utils/ArticleUtils"
+import { useNavigate } from "react-router-dom";
+import { hasRole } from "main/utils/currentUser";
+
+export default function ArticlesTable({ articles, currentUser }) {
+
+    const navigate = useNavigate();
+
+    const editCallback = (cell) => {
+        navigate(`/articles/edit/${cell.row.values.id}`)
+    }
+
+    // Stryker disable all : hard to test for query caching
+    const deleteMutation = useBackendMutation(
+        cellToAxiosParamsDelete,
+        { onSuccess: onDeleteSuccess },
+        ["/api/articles/all"]
+    );
+    // Stryker enable all 
+
+    // Stryker disable next-line all : TODO try to make a good test for this
+    const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
+
+    const columns = [
+        {
+            Header: 'id',
+            accessor: 'id', // accessor is the "key" in the data
+        },
+        {
+            Header: 'Title',
+            accessor: 'title',
+        },
+        {
+            Header: 'URL',
+            accessor: 'url',
+        },
+        {
+            Header: 'Explanation',
+            accessor: 'explanation',
+        },
+        {
+            Header: 'Email',
+            accessor: 'email',
+        },
+        {
+            Header: 'Date',
+            accessor: 'localDateTime',
+        }
+    ];
+
+    const columnsIfAdmin = [
+        ...columns,
+        ButtonColumn("Edit", "primary", editCallback, "ArticlesTable"),
+        ButtonColumn("Delete", "danger", deleteCallback, "ArticlesTable")
+    ];
+
+    const columnsToDisplay = hasRole(currentUser, "ROLE_ADMIN") ? columnsIfAdmin : columns;
+
+    return <OurTable
+        data={articles}
+        columns={columnsToDisplay}
+        testid={"ArticlesTable"}
+    />;
+};
