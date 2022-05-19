e7da98d6e08f2974b61aae06823f3c908370ebfb
Neel Murthy <neelmurthy1@gmail.com>
2022-05-18 20:30:07 -0700

    nm - add HelpRequestTable and modify HelpRequestsIndexPage to include a table instead of a placeholder

--- frontend/src/main/pages/HelpRequests/HelpRequestsIndexPage.js
+++ frontend/src/main/pages/HelpRequests/HelpRequestsIndexPage.js
@@ -1,14 +1,31 @@
+import React from 'react'
+import { useBackend } from 'main/utils/useBackend'; // use prefix indicates a React Hook
+
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import HelpRequestsTable from 'main/components/HelpRequests/HelpRequestsTable';
+import { useCurrentUser } from 'main/utils/currentUser' // use prefix indicates a React Hook
+
 
 export default function HelpRequestsIndexPage() {
+
+  const currentUser = useCurrentUser();
+
+  const { data: helpRequests, error: _error, status: _status } =
+    useBackend(
+      // Stryker disable next-line all : don't test internal caching of React Query
+      ["/api/HelpRequest/all"],
+            // Stryker disable next-line StringLiteral,ObjectLiteral : since "GET" is default, "" is an equivalent mutation
+            { method: "GET", url: "/api/HelpRequest/all" },
+      []
+    );
+
   return (
     <BasicLayout>
       <div className="pt-2">
         <h1>Help Requests</h1>
-        <p>
-          This is where the index page will go
-        </p>
+        <HelpRequestsTable helpRequests={helpRequests} currentUser={currentUser} />
       </div>
     </BasicLayout>
   )
 }
+
